2d4fb51c0b3e2d9a2774f47bea5194f40fb87d4a
anita-mcqs <anita@mcqs.com>
2015-09-08 17:43:48 +0100

    xml parsing

--- MCQS-Android-Version1/app/src/main/java/com/mcqs/anita/mcqs_android_version1/ViewQuestion.java
+++ MCQS-Android-Version1/app/src/main/java/com/mcqs/anita/mcqs_android_version1/ViewQuestion.java
@@ -1,45 +1,85 @@
 package com.mcqs.anita.mcqs_android_version1;
 
-import android.os.AsyncTask;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
+import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import java.io.InputStream;
+import java.util.List;
 
 public class ViewQuestion extends AppCompatActivity {
 
-    private static String questionURL = "http://192.168.1.7:2010/api/fullQuestion";
-    private JSONObject myJSON;
-    private Question myQuestion;
-    private static final String TAG = MainActivity.class.getSimpleName();
+   // private static String questionURL = "http://192.168.1.7:2010/api/fullQuestion";
+   // private Question myQuestion;
+    private TextView backgroundText;
+    private List<Integer> myList;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_question);
+      // myList= listQuestions();//list of xml files
+        parseXML();
+    }
 
+    private void parseXML(){
+        AssetManager assetManager = getBaseContext().getAssets();
         try{
-            myQuestion = new DownloadQuestion().execute(questionURL).get();
+
+            Field[] fields = R.raw.class.getFields();
+
+            int choice= (int) (Math.random() * fields.length);//select random question
+            int resourceID = fields[choice].getInt(fields[choice]);
+            InputStream is=getResources().openRawResource(resourceID);
+
+            //myList.get(choice)
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            SAXParser sp = spf.newSAXParser();
+            XMLReader xr = sp.getXMLReader();
+            QuestionXMLHandler myXMLHandler = new QuestionXMLHandler();
+            xr.setContentHandler(myXMLHandler);
+            InputSource inStream = new InputSource(is);
+            xr.parse(inStream);
+            String background = myXMLHandler.getBackground();
+            String question = myXMLHandler.getQuestion();
+            String core = myXMLHandler.getCore();
+            String explanation = myXMLHandler.getExplanation();
+            ArrayList<QuestionOptions> myOptions = myXMLHandler.getMyOptions();
+            QuestionOptions firstOption = myOptions.get(0);//if boolean true/false
+            String myTest = "my Question: "+ background+ firstOption.getAnswer();
+
+            backgroundText = (TextView) findViewById(R.id.textViewBackground);
+            backgroundText.setText(background + fields.length);
         }
-        catch (InterruptedException e)
-        {
+        catch (Exception e){
             e.printStackTrace();
         }
-        catch (ExecutionException e)
-        {
-            e.printStackTrace();
-        }
+    }
 
-
+    //list of xml files
+    private List<Integer> listQuestions(){
+        List myList = new ArrayList<Integer>();
+        myList.add(R.raw.q1);
+        myList.add(R.raw.q2);
+        myList.add(R.raw.q3);
+        myList.add(R.raw.q4);
+        myList.add(R.raw.q5);
+        myList.add(R.raw.q6);
+        myList.add(R.raw.q7);
+        myList.add(R.raw.q8);
+        myList.add(R.raw.q9);
+        myList.add(R.raw.q10);
+        return myList;
     }
 
     @Override
@@ -64,62 +104,6 @@ public class ViewQuestion extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    private class DownloadQuestion extends AsyncTask<String, Integer, Question>{
-
-        @Override
-        protected Question doInBackground(String... urls){
-            JSONParser jsonParser = new JSONParser();
-            JSONObject myJSON = jsonParser.getJSONFromUrl(questionURL);
-            if(myJSON==null){
-                System.out.println("json null");
-            }
-            Question myQuestion = new Question();
-            try{
-
-                JSONArray jsonArr = myJSON.getJSONArray("optionsArray");
-                String[] optionsArray = new String[jsonArr.length()];//should be 5
-                for(int i=0; i<jsonArr.length(); i++){
-                    optionsArray[i] = (String) jsonArr.get(i);
-                }
-
-                int correctAnswer = myJSON.getInt("correctAnswer");
-                String correctNode = myJSON.getString("correctNode");
-                String background = myJSON.getString("background");
-                String question = myJSON.getString("question");
-                String core = myJSON.getString("core");
-                String explanation = myJSON.getString("explanation");
-                int attemptId = myJSON.getInt("attemptId");
-                String questionId = myJSON.getString("questionId");
-
-                myQuestion.setOptionsArray(optionsArray);
-                myQuestion.setCorrectAnswer(correctAnswer);
-                myQuestion.setCorrectNode(correctNode);
-                myQuestion.setBackground(background);
-                myQuestion.setQuestion(question);
-                myQuestion.setCore(core);
-                myQuestion.setExplanation(explanation);
-                myQuestion.setAttemptId(attemptId);
-                myQuestion.setQuestionId(questionId);
-                System.out.print("json: " + background);
-            }
-
-            catch(JSONException ex){
-                Log.v(TAG, "JSON Exception");
-                ex.printStackTrace();
-            }
-
-
-            return myQuestion;
-        }
-
-        protected void onPostExecute(Question result)
-        {
-            myQuestion = result;
-        }
-
-
-    }
 
 
 }
